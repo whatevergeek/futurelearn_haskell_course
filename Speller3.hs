bb257db7d553910c8a462eeed9a35211648b5bc0
Paul Lorett Amazona <whatevergeek@gmail.com>
2020-10-04 09:02:01 +0800

    refactored/reformat

--- Speller3.hs
+++ Speller3.hs
@@ -4,8 +4,8 @@ spell :: [Char] -> [Char]
 spell (x : xs) = x : " is for " ++ (x : xs)
 
 speller :: [[Char]] -> [Char]
-speller = combine "" . map spell 
- 
+speller = combine "" . map spell
+
 combine :: [Char] -> [[Char]] -> [Char]
 combine _ [] = ""
 combine acc (x : xs)
--- Speller4.hs
+++ Speller4.hs
@@ -1,14 +1,15 @@
 module Speller4 (speller) where
 
-import Data.List
+import Data.List (intercalate)
 
 spell :: [Char] -> [Char]
 spell (x : xs) = x : " is for " ++ (x : xs)
 
 speller :: [[Char]] -> [Char]
-speller = combine . map spell 
+speller = combine . map spell
 
+combine :: [[Char]] -> [Char]
 combine [] = ""
-combine lst 
-    | length lst > 1 = intercalate ", " (init lst) ++ ", and " ++ last lst
-    | otherwise = last lst
+combine lst
+  | length lst > 1 = intercalate ", " (init lst) ++ ", and " ++ last lst
+  | otherwise = last lst
